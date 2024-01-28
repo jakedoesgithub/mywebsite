f86361388323dca5988e70d82baa92b1c47ab8e3
Jake Ardoin <nothxnotsharing@gmail.com>
2024-01-28 15:29:37 -0600

    Redesigned home page, test blog post written, placeholders in blog app

--- home/views.py
+++ home/views.py
@@ -3,4 +3,8 @@ from django.views.generic.base import TemplateView
 
 
 class HomeView(TemplateView):
+    template_name = "home/homepage.html"
+
+
+class IndexView(TemplateView):
     template_name = "home/index.html"
